5eca7701d59c94d4bca2d4ce4b4880f22ea3c257
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-17 13:53:22 +0100

    Update controller.js

--- angular-node/node/controller/controller.js
+++ angular-node/node/controller/controller.js
@@ -1,8 +1,18 @@
-var express = require express();
+var express = require ('express');
+var mongoose = require('mongoose');
+var ProductModel = require('../database/model');
 
-app.use(req, res, next function(){})
+exports.createAThing = (req,res)=>{
+const thing = new Thing({Title:req.body.title,
+name:req.body.name,description:req.body.description,
+});
+thing.save().then(()=>{console.log("data successfully
+Saved!");}).catch((error)=>{
+console.log("data failed ");console.error(error);
+});
 
-export.con = 
+
+}
 
 
 
