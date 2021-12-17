62846a93e25dfc75f53d45b0176f2af181f84106
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-17 17:50:13 +0100

    Update and rename Index.js to app.js

--- angular-node/node/Index.js
+++
@@ -1,6 +0,0 @@
-var express = require express();
-var multer = require multer();
-var bodyParser = require body-parser ();
-var http = require http();
-var session = require session;
-
---
+++ angular-node/node/app.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var session = require ('express-session');
+var app = express();
+
+module.exports = app;
