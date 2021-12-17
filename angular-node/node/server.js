f889e1685c1faee8433edd1aa31ff94fc2cff2f9
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-17 17:27:48 +0100

    Update server.js

--- angular-node/node/server.js
+++ angular-node/node/server.js
@@ -1,1 +1,9 @@
+var http = require('http');
+var app = require('/app');
+var port = 3000;
 
+var server = http.createServer(req,res,function(){
+res.end();
+}
+server.listen(port, function(){"server is listening on
+port 3000";});
