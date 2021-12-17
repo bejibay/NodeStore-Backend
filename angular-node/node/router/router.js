9d2d4c2803454bf62dc4f0aedeacebffba322d78
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-17 17:11:43 +0100

    Update router.js

--- angular-node/node/router/router.js
+++ angular-node/node/router/router.js
@@ -1,2 +1,8 @@
 var express = require express();
 var router = express.Router();
+var stuffRoute = require('../controllers/controllers');
+
+Router.post('/','createOneThing.stuffRoute');
+Router.get('/:id','getOneThing.stuffRoute');
+Router.put('/','modifyOneThing.stuffRoute');
+Router.get('/','getAllThings.stuffRoute');
