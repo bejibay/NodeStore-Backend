97e39c49b23641cf5a35d5564c035a81c2b83470
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-17 16:57:43 +0100

    Update controller.js

--- angular-node/node/controller/controller.js
+++ angular-node/node/controller/controller.js
@@ -1,18 +1,31 @@
 var express = require ('express');
 var mongoose = require('mongoose');
-var ProductModel = require('../database/model');
+var ProductModel = require('../database/dbase');
 
-exports.createAThing = (req,res)=>{
-const thing = new Thing({Title:req.body.title,
-name:req.body.name,description:req.body.description,
+exports.createOneThing = (req,res)=>{
+const thing = new Thing({title:req.body.title,
+description:req.body.description,quantity:req.body.quantity,
+price:req.body.price,imageurl:req.body.imageurl
 });
-thing.save().then(()=>{console.log("data successfully
-Saved!");}).catch((error)=>{
-console.log("data failed ");console.error(error);
+thing.save().then(()=>{res.status(200).json({message:"data
+successfully saved!"});}).catch((error)=>{
+res.status(400).json({error:error});
 });
-
-
 }
-
-
-
+exports.getOneThing = (req,res)=>{
+Thing.findOne(_id;req.params.id).then((thing)
+=>{res.status(200).json(thing);});
+};
+exports.modifyOneThing=(req,res)=>{
+const thing = new Thing(_id:req.params.id,
+title:req.body.title,description:req.body.description,
+quantity;req.body.quantity,price:req.body.price,imageurl:req.body.imageurl
+);
+Thing.updateOne(_id:req.params.id,thing).then(()=>{res.status(200).json
+({message:"successfully updated"});
+}).catch((error)=>{res.status(404).json({error;"error"});});
+};
+exports.getAllthings = (req,res)=>{
+Thing.find().then((things)=>{res.status(200).json.(things);}).
+catch((error)=>{res.status(400).json({error:"error"});});
+};
