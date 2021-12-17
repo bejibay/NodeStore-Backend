f4ca6201454d3d7d27337a66183eb1f9af75bd23
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-17 17:22:17 +0100

    Update Index.js

--- angular-node/node/Index.js
+++ angular-node/node/Index.js
@@ -4,11 +4,3 @@ var bodyParser = require body-parser ();
 var http = require http();
 var session = require session;
 
-var app = express;
-var port = 3000;
-var app = express;
-http.createServer(req,res,function(){
-res.end();
-}
-server.listen(port, function(){"server is listening on
-port 3000";});
