b9b57e4d13307adcec5f520780957cac36cb513a
bejibay <58639255+bejibay@users.noreply.github.com>
2022-03-22 15:33:42 +0100

    Create router.js

---
+++ router/router.js
@@ -0,0 +1,10 @@
+var express = require express();
+var router = express.Router();
+var stuffRoute = require('../controllers/controllers');
+
+Router.post('/','createOneThing.stuffRoute');
+Router.get('/:id','getOneThing.stuffRoute');
+Router.put('/','modifyOneThing.stuffRoute');
+Router.get('/','getAllThings.stuffRoute');
+Router.delete('/:id','deleteOneThing');
+module.exports = router;
